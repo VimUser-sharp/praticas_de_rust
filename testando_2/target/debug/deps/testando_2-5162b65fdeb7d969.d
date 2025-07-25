d3423ac6dde444bbcd75005bfe97c5dbe7154283
VimUser-sharp <fronterlinemail@tutamail.com>
2025-07-25 16:49:18 -0300

    Delete testando_2/target/debug/deps directory

--- testando_2/target/debug/deps/testando_2-5162b65fdeb7d969.d
+++
@@ -1,5 +0,0 @@
-/home/joker/Documentos/codigos/neovim codes/testando_2/target/debug/deps/testando_2-5162b65fdeb7d969.d: src/main.rs
-
-/home/joker/Documentos/codigos/neovim codes/testando_2/target/debug/deps/libtestando_2-5162b65fdeb7d969.rmeta: src/main.rs
-
-src/main.rs:
